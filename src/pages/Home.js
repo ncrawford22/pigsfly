10037313458c3b9715e1b9faa936d2b23f50bd1d
Nate Crawford <nathanwcrawford22@gmail.com>
2022-11-09 19:51:06 -0500

    More minor CSS fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="home">
-            <h1>Pigsfly Fantasy Football</h1>
+            <h1 className="home-h1">Pigsfly Fantasy Football</h1>
             <img alt="logo" src="./logo-512.png"></img>
             <h2 className="home-h2">"Where Fantasy Sports Meets Reality?"</h2>
         </div>
