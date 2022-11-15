1a434248dbaa938142732b3f18ddabc907bfdb2b
Nate Crawford <nathanwcrawford22@gmail.com>
2022-11-15 15:08:37 -0500

    Refactored code, API data manipulation updated, minor CSS updates

--- src/components/DisplayFantasyData.js
+++ src/components/DisplayFantasyData.js
@@ -1,31 +1,23 @@
-import { useEffect, useState } from "react"
-
-export default function DisplayFantasyData() {
-
-    let [fantasyDataMain, setFantasyDataMain] = useState([]);
-   
-    const URL = 'https://fantasy.espn.com/apis/v3/games/ffl/seasons/2022/segments/0/leagues/387714?view=mMatchup&view=mMatchupScore&view=mRoster&view=mScoreboard&view=mSettings&view=mStatus&view=mTeam&view=modular&view=mNav';
-    
-
-    const getFantasyDataMain = async () => {
-        try {
-          const response = await fetch(URL);
-          const data = await response.json();
-          setFantasyDataMain(data)
-        } catch (error) {
-            alert(error)
-        }
-    }
-
-    useEffect(() => {
-            getFantasyDataMain();
-    }, [])
-
-    console.log('this is fantasyDataMain', fantasyDataMain.teams)
-
+export default function DisplayFantasyData({ team, fantasyDataMain, teamName, email}) {
     return (
         <div>
-
+            <h1 style={{paddingBottom: '10px'}}>{teamName}</h1>
+            {fantasyDataMain && 
+                <div className="profile-logo" style={{display: 'flex', justifyContent: 'center'}}>
+                    <img src={team.logo} alt="profile-logo"></img>
+                </div>}
+                <br />
+            <p style={{fontWeight: 'bolder'}}>Team Name: <span style={{fontWeight: 'normal'}}>{teamName}</span></p>
+            <p>Email: {email}</p>
+            <div className="record">Record: {team.record.overall.wins}-{team.record.overall.losses}-{team.record.overall.ties}</div>
+            {team.record.overall.streakType === "WIN" && <div className="winStreak">Winning Streak: {team.record.overall.streakLength} Wins</div>}
+            {team.record.overall.streakType === "LOSS" && <div className="loseStreak">Losing Streak: {team.record.overall.streakLength} Losses</div>}
+            <div className="playoffSeed">Current Projected Playoff Seed: #{team.playoffSeed}</div>
+            <br />
+            <div className="roster">
+                <h5 style={{textDecoration: 'underline'}}>Current Roster</h5>
+                {team.roster.entries.map((entry, i) => <div key={i}>{entry.playerPoolEntry.player.fullName}</div>)}
+            </div>
         </div> 
     )
 }
--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
 import DisplayFantasyData from "../components/DisplayFantasyData";
 
 function TeamPage({ teamName, email }) {
+    
+    let [fantasyDataMain, setFantasyDataMain] = useState([]);
+    let [team, setTeam] = useState({});
+    let [isLoading, setIsLoading] = useState(true);
+
+    const URL = 'https://fantasy.espn.com/apis/v3/games/ffl/seasons/2022/segments/0/leagues/387714?view=mMatchup&view=mMatchupScore&view=mRoster&view=mScoreboard&view=mSettings&view=mStatus&view=mTeam&view=modular&view=mNav';
+
+useEffect(() => {
+    const getFantasyDataMain = async () => {
+        try {
+          setIsLoading(true) 
+          const response = await fetch(URL);
+          const data = await response.json();
+          console.log('Check here for full data', data)
+          setFantasyDataMain(data)
+          const teams = data.teams.find( team =>
+            team.name === teamName)
+            setTeam(teams)
+            setIsLoading(false)
+        } catch (error) {
+            alert(error)
+            setIsLoading(false)
+        }
+    }
+    getFantasyDataMain();
+}, [teamName])
+
+console.log(team)
     return ( 
         <div className="team-page">
-            <h1>{teamName}</h1>
-            <p>Team Name: {teamName}</p>
-            <p>Email: {email}</p>
-            <DisplayFantasyData teamName={teamName} />
+            {isLoading && <h1>Loading Fantasy data....</h1>}
+            {!isLoading && <DisplayFantasyData team={team} teamName={teamName} isLoading={isLoading} fantasyDataMain={fantasyDataMain} email={email} />}
         </div>
         
      );
