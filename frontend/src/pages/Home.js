3cb8e717e8f3c9d8318292f7dea0ba552047ffb7
Nate Crawford <nathanwcrawford22@gmail.com>
2022-11-16 16:21:27 -0500

    More updates

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,10 @@ export default function Home() {
         <div className="home">
             <h1 className="home-h1" style={{paddingTop: '10px'}}>Pigsfly Fantasy Football</h1>
             <a href="https://fantasy.espn.com/football/league?leagueId=387714" target="_blank" rel="noreferrer"><img className="logo"alt="logo" src="./logo-512.png"></img></a>
-            <h2 className="home-h2">"Where Fantasy Sports Meets Reality?"</h2>
+            <div className="home-h2-outerBorder"></div>
+            <div className="home-h2-border">
+                <h2 className="home-h2">"Where Fantasy Sports Meets Reality?"</h2>
+            </div>
         </div>
     )
 }
